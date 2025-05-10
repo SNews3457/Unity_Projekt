be83b16304d6aab7e4aa430ad538222c38bdcf0b
CoderDagobert <sauron20202010@gmail.com>
2025-05-10 13:57:08 +0200

    SkillUI Slider
    Slider zur punkt level anzeige

---
+++ Unity_Projekt/Assets/Scripts/LevelSystem/LevelUpManager.cs
@@ -0,0 +1,17 @@
+//op Dagobert
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class LevelUpManager : MonoBehaviour
+{
+    public float LevelPoints = 0; //aktuele LevelPunkte
+    public float PointsNeeded = 2; //Punkte die für den Levelaufstieg benötigt werden
+    public float Level = 0; //op aktuels Level
+    public Slider LevelAmount;
+    public TMPro.TMP_Text Points;
+    void Update()
+    {
+        LevelAmount.value = LevelPoints;
+        Points.text = "LevelUp:       " + LevelPoints + "/" + PointsNeeded;
+    }
+}
