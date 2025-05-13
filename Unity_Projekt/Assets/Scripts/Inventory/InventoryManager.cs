71f0961bb7156b377a6dfd5c020253d1b62c8c1d
CoderDagobert <sauron20202010@gmail.com>
2025-05-13 18:13:12 +0200

    Item storing im Inventar technisch gesehen möglich

--- Unity_Projekt/Assets/Scripts/Inventory/InventoryManager.cs
+++ Unity_Projekt/Assets/Scripts/Inventory/InventoryManager.cs
@@ -3,27 +3,58 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using System.ComponentModel;
+using UnityEngine.UI;
 public class InventoryManager : MonoBehaviour
 {
-    public ItemClass itemToAdd;
+    [SerializeField] private GameObject slotHolder;
+    [SerializeField] private ItemClass itemToAdd;
     public GameObject inventory;
     public bool InventoryisActiv;
-    public List<ItemClass> items = new List<ItemClass>();
-    public ItemClass itemToRemove;
+    [SerializeField] private List<ItemClass> items = new List<ItemClass>();
+    [SerializeField] private ItemClass itemToRemove;
+
+    private GameObject[] slots;
 
     private void Start()
     {
+        slots = new GameObject[slotHolder.transform.childCount];
+        //Dagobert Slots zuweisen
+        for (int i = 0; i < slotHolder.transform.childCount; i++)
+        {
+            slots[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+
+        RefreshUI();
         Add(itemToAdd);
+    }
+
+    public void RefreshUI()
+    {
+        for (int i = 0;i < slots.Length;i++) 
+        {
+            try
+            {
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].itemIcon;
+            }
+            catch
+            {
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+            }
+        }
     }
 
     public void Add(ItemClass item)
     {
         items.Add(item);
+        RefreshUI();
     }
 
     public void Remove(ItemClass item)
     {
         items.Remove(item);
+        RefreshUI();
     }
 
     private void Update()
