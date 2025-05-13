0a7ab13e40c5513d6c68fcc5f5ecbc3c23e82361
CoderDagobert <sauron20202010@gmail.com>
2025-05-13 17:56:16 +0200

    ScriaptableObjects für Items

---
+++ Unity_Projekt/Assets/Scripts/Inventory/ItemClass.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public abstract class ItemClass : ScriptableObject
+{
+    [Header("Item")]
+    public string itemName;
+    public Sprite itemIcon;
+
+    public abstract ItemClass GetItem();
+    public abstract ToolClass GetTool();
+    public abstract MaterialClass GetMaterial();
+}
