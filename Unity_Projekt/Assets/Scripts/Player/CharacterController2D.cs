51fb2ed15bfb330cd6e3857e566985bf6d2da592
CoderDagobert <sauron20202010@gmail.com>
2025-06-20 14:37:05 +0200

    auschalten der coyote time aufgrund eines bugs

--- Unity_Projekt/Assets/Scripts/Player/CharacterController2D.cs
+++ Unity_Projekt/Assets/Scripts/Player/CharacterController2D.cs
@@ -5,11 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class CharacterController2D : MonoBehaviour
 {
-    [SerializeField] private float m_JumpForce = 400f;
-    [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;
-    [SerializeField] private bool m_AirControl = false;
-    [SerializeField] private LayerMask m_WhatIsGround;
-    [SerializeField] private Transform m_GroundCheck;
+    [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
+    [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;   // How much to smooth out the movement
+    [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
+    [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
+    [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
     [SerializeField] private Transform m_WallCheck;
 
     public AudioClip teleportSound;
@@ -19,45 +19,46 @@ public class CharacterController2D : MonoBehaviour
     public SpriteRenderer spriteRenderer;
     public Camera mainCamera;
     private bool isTeleporting = false;
+    //Posicion que controla si el personaje toca una pared
 
     private Transform lastCheckpoint;
     private SaveGroundSaver savegroundsaver;
     public int lives = 5;
-    public Transform checkpoint;
+    public Transform checkpoint; //SNews: Letzter Checkpoint
     public bool CheckpointActive = false;
 
-    const float k_GroundedRadius = .2f;
-    private bool m_Grounded;
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool m_Grounded;            // Whether or not the player is grounded.
     private Rigidbody2D m_Rigidbody2D;
-    private bool m_FacingRight = true;
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
     private Vector3 velocity = Vector3.zero;
-    private float limitFallSpeed = 25f;
+    private float limitFallSpeed = 25f; // Limit fall speed
 
     public bool SkillDoubkeJump;
-    public bool canDoubleJump = true;
+    public bool canDoubleJump = true; //If player can double jump
     public float m_DashForce = 25f;
     private bool canDash = true;
-    private bool isDashing = false;
-    private bool m_IsWall = false;
-    private bool isWallSliding = false;
-    private bool oldWallSlidding = false;
+    private bool isDashing = false; //If player is dashing
+    private bool m_IsWall = false; //If there is a wall in front of the player
+    private bool isWallSliding = false; //If player is sliding in a wall
+    private bool oldWallSlidding = false; //If player is sliding in a wall in the previous frame
     private float prevVelocityX = 0f;
-    private bool canCheck = false;
+    private bool canCheck = false; //For check if player is wallsliding
 
-    public float life = 10f;
-    public bool invincible = false;
-    private bool canMove = true;
+    public float life = 10f; //Life of the player
+    public bool invincible = false; //If player can die
+    private bool canMove = true; //If player can move
 
     private Animator animator;
-    public ParticleSystem particleJumpUp;
-    public ParticleSystem particleJumpDown;
+    public ParticleSystem particleJumpUp; //Trail particles
+    public ParticleSystem particleJumpDown; //Explosion particles
+
     public ParticleSystem particleJumpWall;
 
     public CurrencyManager currencyManager;
     private float jumpWallStartX = 0;
-    private float jumpWallDistX = 0;
-    private bool limitVelOnWallJump = false;
-
+    private float jumpWallDistX = 0; //Distance between player and wall
+    private bool limitVelOnWallJump = false; //For limit wall jump distance with low fps
     [Header("Events")]
     [Space]
     public Sprite UnactiveCheckpoint;
@@ -69,16 +70,12 @@ public class CharacterController2D : MonoBehaviour
     [System.Serializable]
     public class BoolEvent : UnityEvent<bool> { }
 
-    // === Coyote Time ===
-    [SerializeField] private float coyoteTime = 0.2f;
-    private float coyoteTimeCounter;
-
     private void Awake()
     {
         savegroundsaver = GetComponent<SaveGroundSaver>();
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        audioSource = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();  // AudioSource holen
 
         if (OnFallEvent == null)
             OnFallEvent = new UnityEvent();
@@ -87,32 +84,33 @@ public class CharacterController2D : MonoBehaviour
             OnLandEvent = new UnityEvent();
     }
 
+
+
     private void FixedUpdate()
     {
         if (!SkillDoubkeJump)
+        {
             canDoubleJump = false;
-
+        }
         life = 1;
         bool wasGrounded = m_Grounded;
         m_Grounded = false;
-        coyoteTimeCounter -= Time.fixedDeltaTime;
 
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
         Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
         for (int i = 0; i < colliders.Length; i++)
         {
             if (colliders[i].gameObject != gameObject)
-            {
                 m_Grounded = true;
-                coyoteTimeCounter = coyoteTime;
-                if (!wasGrounded)
-                {
-                    OnLandEvent.Invoke();
-                    if (!m_IsWall && !isDashing)
-                        particleJumpDown.Play();
-                    canDoubleJump = true;
-                    if (m_Rigidbody2D.linearVelocity.y < 0f)
-                        limitVelOnWallJump = false;
-                }
+            if (!wasGrounded)
+            {
+                OnLandEvent.Invoke();
+                if (!m_IsWall && !isDashing)
+                    particleJumpDown.Play();
+                canDoubleJump = true;
+                if (m_Rigidbody2D.linearVelocity.y < 0f)
+                    limitVelOnWallJump = false;
             }
         }
 
@@ -137,7 +135,6 @@ public class CharacterController2D : MonoBehaviour
         {
             if (m_Rigidbody2D.linearVelocity.y < -0.5f)
                 limitVelOnWallJump = false;
-
             jumpWallDistX = (jumpWallStartX - transform.position.x) * transform.localScale.x;
             if (jumpWallDistX < -0.5f && jumpWallDistX > -1f)
             {
@@ -148,7 +145,12 @@ public class CharacterController2D : MonoBehaviour
                 canMove = true;
                 m_Rigidbody2D.linearVelocity = new Vector2(10f * transform.localScale.x, m_Rigidbody2D.linearVelocity.y);
             }
-            else if (jumpWallDistX < -2f || jumpWallDistX > 0)
+            else if (jumpWallDistX < -2f)
+            {
+                limitVelOnWallJump = false;
+                m_Rigidbody2D.linearVelocity = new Vector2(0, m_Rigidbody2D.linearVelocity.y);
+            }
+            else if (jumpWallDistX > 0)
             {
                 limitVelOnWallJump = false;
                 m_Rigidbody2D.linearVelocity = new Vector2(0, m_Rigidbody2D.linearVelocity.y);
@@ -156,35 +158,48 @@ public class CharacterController2D : MonoBehaviour
         }
     }
 
+
     public void Move(float move, bool jump, bool dash)
     {
         if (canMove)
         {
             if (dash && canDash && !isWallSliding)
+            {
+                //m_Rigidbody2D.AddForce(new Vector2(transform.localScale.x * m_DashForce, 0f));
                 StartCoroutine(DashCooldown());
-
+            }
+            // If crouching, check to see if the character can stand up
             if (isDashing)
             {
                 m_Rigidbody2D.linearVelocity = new Vector2(transform.localScale.x * m_DashForce, 0);
             }
+            //only control the player if grounded or airControl is turned on
             else if (m_Grounded || m_AirControl)
             {
                 if (m_Rigidbody2D.linearVelocity.y < -limitFallSpeed)
                     m_Rigidbody2D.linearVelocity = new Vector2(m_Rigidbody2D.linearVelocity.x, -limitFallSpeed);
-
+                // Move the character by finding the target velocity
                 Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.linearVelocity.y);
+                // And then smoothing it out and applying it to the character
                 m_Rigidbody2D.linearVelocity = Vector3.SmoothDamp(m_Rigidbody2D.linearVelocity, targetVelocity, ref velocity, m_MovementSmoothing);
 
+                // If the input is moving the player right and the player is facing left...
                 if (move > 0 && !m_FacingRight && !isWallSliding)
+                {
+                    // ... flip the player.
                     Flip();
+                }
+                // Otherwise if the input is moving the player left and the player is facing right...
                 else if (move < 0 && m_FacingRight && !isWallSliding)
+                {
+                    // ... flip the player.
                     Flip();
+                }
             }
-
-            // === Coyote Time Jump ===
-            if ((m_Grounded || coyoteTimeCounter > 0f) && jump)
+            // If the player should jump...
+            if (m_Grounded && jump)
             {
-                coyoteTimeCounter = 0f;
+                // Add a vertical force to the player.
                 animator.SetBool("IsJumping", true);
                 animator.SetBool("JumpUp", true);
                 m_Grounded = false;
@@ -201,16 +216,78 @@ public class CharacterController2D : MonoBehaviour
                 animator.SetBool("IsDoubleJumping", true);
             }
 
-            // Wall Sliding & Wall Jumping
-            // (Hier bleibt dein bestehender Code unverändert)
-            // ...
-            // Code für wall sliding und jumping wie zuvor
+            else if (m_IsWall && !m_Grounded)
+            {
+                if (!oldWallSlidding && m_Rigidbody2D.linearVelocity.y < 0 || isDashing)
+                {
+                    isWallSliding = true;
+                    m_WallCheck.localPosition = new Vector3(-m_WallCheck.localPosition.x, m_WallCheck.localPosition.y, 0);
+                    Flip();
+                    StartCoroutine(WaitToCheck(0.1f));
+                    canDoubleJump = true;
+                    animator.SetBool("IsWallSliding", true);
+                }
+                isDashing = false;
+
+                if (isWallSliding)
+                {
+                    if (move * transform.localScale.x > 0.1f)
+                    {
+                        StartCoroutine(WaitToEndSliding());
+                    }
+                    else
+                    {
+                        oldWallSlidding = true;
+                        m_Rigidbody2D.linearVelocity = new Vector2(-transform.localScale.x * 2, -5);
+                    }
+                }
+
+                if (jump && isWallSliding)
+                {
+                    animator.SetBool("IsJumping", true);
+                    animator.SetBool("JumpUp", true);
+                    m_Rigidbody2D.linearVelocity = new Vector2(0f, 0f);
+                    m_Rigidbody2D.AddForce(new Vector2(transform.localScale.x * m_JumpForce * 1.2f, m_JumpForce));
+                    jumpWallStartX = transform.position.x;
+                    limitVelOnWallJump = true;
+                    canDoubleJump = true;
+                    isWallSliding = false;
+                    animator.SetBool("IsWallSliding", false);
+                    oldWallSlidding = false;
+                    m_WallCheck.localPosition = new Vector3(Mathf.Abs(m_WallCheck.localPosition.x), m_WallCheck.localPosition.y, 0);
+                    canMove = false;
+                    particleJumpWall.Play();
+                    //particleJumpDown.Play();
+                    //particleJumpUp.Play();
+                }
+                else if (dash && canDash)
+                {
+                    isWallSliding = false;
+                    animator.SetBool("IsWallSliding", false);
+                    oldWallSlidding = false;
+                    m_WallCheck.localPosition = new Vector3(Mathf.Abs(m_WallCheck.localPosition.x), m_WallCheck.localPosition.y, 0);
+                    canDoubleJump = true;
+                    StartCoroutine(DashCooldown());
+                }
+            }
+            else if (isWallSliding && !m_IsWall && canCheck)
+            {
+                isWallSliding = false;
+                animator.SetBool("IsWallSliding", false);
+                oldWallSlidding = false;
+                m_WallCheck.localPosition = new Vector3(Mathf.Abs(m_WallCheck.localPosition.x), m_WallCheck.localPosition.y, 0);
+                canDoubleJump = true;
+            }
         }
     }
 
+
     private void Flip()
     {
+        // Switch the way the player is labelled as facing.
         m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
@@ -236,30 +313,37 @@ public class CharacterController2D : MonoBehaviour
             }
         }
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)//SNews
     {
         if (other.CompareTag("Checkpoint"))
         {
             SpriteRenderer newCheckpointSprite = other.GetComponent<SpriteRenderer>();
+
+            // Deaktiviere den alten Checkpoint (wenn vorhanden)
             if (lastCheckpoint != null)
             {
                 SpriteRenderer lastCheckpointSprite = lastCheckpoint.GetComponent<SpriteRenderer>();
                 if (lastCheckpointSprite != null)
+                {
                     lastCheckpointSprite.sprite = UnactiveCheckpoint;
+                }
             }
+
+            // Aktiviere neuen Checkpoint
             newCheckpointSprite.sprite = ActivCheckpoint;
+            Debug.Log("Checkpoint aktiviert!");
             checkpoint = other.transform;
-            lastCheckpoint = other.transform;
+            lastCheckpoint = other.transform; // Setze neuen als letzten aktiven
             CheckpointActive = true;
         }
 
         if (other.CompareTag("Orb"))
         {
-            currencyManager.AddOrbs(1);
+            currencyManager.AddOrbs(1); // Orbs sicher hinzufügen und speichern
             Destroy(other.gameObject);
         }
     }
+
 
     IEnumerator DashCooldown()
     {
@@ -278,12 +362,34 @@ public class CharacterController2D : MonoBehaviour
         yield return new WaitForSeconds(time);
         canMove = true;
     }
-
     IEnumerator MakeInvincible(float time)
     {
         invincible = true;
         yield return new WaitForSeconds(time);
         invincible = false;
+    }
+    IEnumerator WaitToMove(float time)
+    {
+        canMove = false;
+        yield return new WaitForSeconds(time);
+        canMove = true;
+    }
+
+    IEnumerator WaitToCheck(float time)
+    {
+        canCheck = false;
+        yield return new WaitForSeconds(time);
+        canCheck = true;
+    }
+
+    IEnumerator WaitToEndSliding()
+    {
+        yield return new WaitForSeconds(0.1f);
+        canDoubleJump = true;
+        isWallSliding = false;
+        animator.SetBool("IsWallSliding", false);
+        oldWallSlidding = false;
+        m_WallCheck.localPosition = new Vector3(Mathf.Abs(m_WallCheck.localPosition.x), m_WallCheck.localPosition.y, 0);
     }
 
     IEnumerator WaitToDead()
@@ -310,9 +416,13 @@ public class CharacterController2D : MonoBehaviour
 
         if (CheckpointActive && checkpoint != null && lives == 0)
         {
+            rb.simulated = true;
+
+            collider.enabled = true;
+            Debug.Log("Hat geklappt");
             transform.position = checkpoint.position;
             animator.SetBool("IsDead", false);
-            life = 10f;
+            life = 10f; //SNews Reset der Leben
             canMove = true;
             lives = 5;
             invincible = false;
@@ -320,10 +430,15 @@ public class CharacterController2D : MonoBehaviour
         }
         else if (lives == 0)
         {
-            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+            rb.simulated = true;
+            collider.enabled = true;
+            Debug.Log("Checkpoint nicht aktiv oder nicht vorhanden!");
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex); //SNews: Fallback falls kein Checkpoint gesetzt
         }
+
         yield return new WaitForSeconds(1);
         invincible = false;
+
     }
 
     public void TeleportForward()
@@ -337,26 +452,41 @@ public class CharacterController2D : MonoBehaviour
         isTeleporting = true;
         canMove = false;
 
+        // Sound abspielen
         if (audioSource != null && teleportSound != null)
+        {
             audioSource.PlayOneShot(teleportSound);
+        }
 
+        // Unsichtbar machen
         spriteRenderer.enabled = false;
 
+        // Kamera deaktivieren
         if (mainCamera != null)
             mainCamera.GetComponent<CameraFollow>().enabled = false;
 
+        // Richtung abhängig von Blickrichtung
         float direction = m_FacingRight ? 1f : -1f;
         Vector3 teleportTarget = transform.position + new Vector3(direction * teleportDistance, 0f, 0f);
 
+        // Kurze Wartezeit für Effekt (optional)
         yield return new WaitForSeconds(teleportDuration);
+
+        // Position setzen
         transform.position = teleportTarget;
 
+        // Kamera folgt wieder
         yield return new WaitForSeconds(0.05f);
         if (mainCamera != null)
             mainCamera.GetComponent<CameraFollow>().enabled = true;
 
+        // Sichtbar machen
         spriteRenderer.enabled = true;
+
         isTeleporting = false;
         canMove = true;
     }
+
+
+
 }
